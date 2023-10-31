1d2ddea0c815231068f566b8cc37d358716efdf8
Beatriz Vitoria <beatrizvitoria519@gmail.com>
2023-10-31 14:03:15 -0400

    Nova Versão

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,23 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const carousel = document.querySelector(".carousel")
-  const prevBtn = document.querySelector(".prev")
-  const nextBtn = document.querySelector(".next")
+"use strict"
 
-  let currentIndex = 0
+/**
+ * element toggle function
+ */
 
-  prevBtn.addEventListener("click", () => {
-    currentIndex =
-      (currentIndex - 1 + carousel.children.length) % carousel.children.length
-    updateCarousel()
-  })
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active")
+}
 
-  nextBtn.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % carousel.children.length
-    updateCarousel()
-  })
+/**
+ * header sticky & go to top
+ */
 
-  function updateCarousel() {
-    const translateX = currentIndex * -100
-    carousel.style.transform = `translateX(${translateX}%)`
+const header = document.querySelector("[data-header]")
+const goTopBtn = document.querySelector("[data-go-top]")
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 10) {
+    header.classList.add("active")
+    goTopBtn.classList.add("active")
+  } else {
+    header.classList.remove("active")
+    goTopBtn.classList.remove("active")
   }
 })
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]")
+const navbar = document.querySelector("[data-navbar]")
+
+navToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(navToggleBtn)
+  elemToggleFunc(navbar)
+  elemToggleFunc(document.body)
+})
+
+/**
+ * dark & light theme toggle
+ */
+
+const themeToggleBtn = document.querySelector("[data-theme-btn]")
+
+themeToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(themeToggleBtn)
+
+  if (themeToggleBtn.classList.contains("active")) {
+    document.body.classList.remove("dark_theme")
+    document.body.classList.add("light_theme")
+
+    localStorage.setItem("theme", "light_theme")
+  } else {
+    document.body.classList.add("dark_theme")
+    document.body.classList.remove("light_theme")
+
+    localStorage.setItem("theme", "dark_theme")
+  }
+})
+
+/**
+ * check & apply last time selected theme from localStorage
+ */
+
+if (localStorage.getItem("theme") === "light_theme") {
+  themeToggleBtn.classList.add("active")
+  document.body.classList.remove("dark_theme")
+  document.body.classList.add("light_theme")
+} else {
+  themeToggleBtn.classList.remove("active")
+  document.body.classList.remove("light_theme")
+  document.body.classList.add("dark_theme")
+}
